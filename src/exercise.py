2baf282123c1dbc1cc86648dd97cdfcbf87cc0ac
cabosh <callum.bosher@gmail.com>
2020-06-04 22:21:42 +0100

    Attempted commit

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #write your code below this line
 
+    while True:
+
+        carry_on = input("Carry on? ")
+
+        if carry_on == "no":
+            break
+
 if __name__ == '__main__':
     main()
